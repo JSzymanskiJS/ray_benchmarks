e97fc7c07e846ff6301d7dc637dbf4cf2b653001
JSzymanskiJS <jszymanskijs@protonmail.com>
2023-05-27 20:39:11 +0200

    Update

---
+++ ray_benchmarks/src/bash_scripts/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+conda update conda -y
+conda info
+conda create --name ray_benchmarks python=3.10 -y
+conda activate ray_benchmarks
+conda install jupyter -y
+pip install torch torchvision torchaudio
+pip install numpy pandas boto3 tqdm tabulate
+pip install -U "ray[default]"
